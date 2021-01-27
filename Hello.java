3590f3c85852471e7a1221c729b5b62d63d6a9d9
hollandro <78064580+hollandro@users.noreply.github.com>
2021-01-26 23:21:05 -0500

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,9 @@
+public class Hello {
+	
+	public static void main(String[] args) {
+
+	System.out.println("<Your Name>");
+	System.out.println("<Java Project Name>");
+	System.out.println();
+    }
+}
