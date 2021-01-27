e955064fe0e2ef418439a180f89f929b3903b797
hollandro <78064580+hollandro@users.noreply.github.com>
2021-01-27 12:08:18 -0500

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,21 @@
+/*****************************************
+*Java Program Name
+******************************************
+* Ronald Holland
+*01/26/21
+*CMSC-255-03
+*****************************************/
+
+
+public class Hello {
+	
+	public static void main(String[] args) {
+
+	System.out.println("<Ronald Holland>");
+	System.out.println("<HelloWorld>");
+	System.out.println("<Lab 1>");
+	System.out.println("<01/26/21>");
+	System.out.println("<CMSC-255-03>");
+	System.out.println();
+    }
+}
