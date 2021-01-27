4d162e51d4ea66c0ad383523033a9f9cd75a10f7
hollandro <78064580+hollandro@users.noreply.github.com>
2021-01-27 12:06:18 -0500

    Delete Hello.java

--- Hello.java
+++
@@ -1,9 +0,0 @@
-public class Hello {
-	
-	public static void main(String[] args) {
-
-	System.out.println("<Your Name>");
-	System.out.println("<Java Project Name>");
-	System.out.println();
-    }
-}
